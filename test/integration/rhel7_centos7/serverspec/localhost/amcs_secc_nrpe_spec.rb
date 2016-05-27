b68e25f106a94e786e1daeb011664331d230c4eb
crgr <c.grau@telekom.de>
2016-05-27 16:40:02 +0200

    commit templates nrpe.cfg.erb and nrpe.erb commit tests amcs_secc_nrpe_spec.rb

---
+++ test/integration/rhel7_centos7/serverspec/localhost/amcs_secc_nrpe_spec.rb
@@ -0,0 +1,92 @@
+require 'spec_helper'
+
+	describe user('nrpe') do
+  		it { should exist }
+  		it { should have_home_directory '/home/nrpe' }
+  		it { should have_login_shell '/sbin/nologin' }
+	end
+	
+	describe user('nagios') do
+  		it { should exist }
+  		it { should have_home_directory '/var/spool/nagios' }
+  		it { should have_login_shell '/sbin/nologin' }
+	end
+
+	describe package ('nrpe') do
+		it { should be_installed }
+	end
+	
+	describe package ('sudo') do
+		it { should be_installed }
+	end
+	
+	describe service ('nrpe') do
+		it { should be_enabled }
+		it { should be_running }
+	end
+	
+	describe file('/etc/nagios/') do
+		it { should exist }
+		it { should be_directory }
+		it { should be_mode 755 }
+		it { should be_owned_by 'root' }
+		it { should be_grouped_into 'root'}		
+	end
+	
+	describe file('/home/nrpe/bin/') do
+		it { should exist }
+		it { should be_directory }
+		it { should be_mode 750 }
+		it { should be_owned_by 'nrpe' }
+		it { should be_grouped_into 'nrpe'}
+	end
+	
+	describe file('/var/run/nrpe') do
+		it { should exist }
+		it { should be_directory }
+		it { should be_mode 755 }
+		it { should be_owned_by 'nrpe' }
+		it { should be_grouped_into 'nrpe'}		
+	end
+	
+	describe file('/etc/nrpe.d/') do
+		it { should exist }
+		it { should be_directory }
+		it { should be_mode 755 }
+		it { should be_owned_by 'root' }
+		it { should be_grouped_into 'root'}		
+	end
+	
+	describe file('/etc/sudoers.d/nrpe') do
+		it { should exist }
+		it { should be_mode 440 }
+		it { should be_owned_by 'root' }
+		it { should be_grouped_into 'root' }
+		its(:content) { should match /^Defaults\:nrpe \!requiretty$/ }
+	end
+	
+	describe file('/etc/nrpe.d/general.cfg') do
+		it { should exist }
+		it { should be_mode 644 }
+		it { should be_owned_by 'root' }
+		it { should be_grouped_into 'root' }
+	end
+	
+	describe file('/etc/nagios/nrpe.cfg') do
+		it { should exist }
+		it { should be_mode 644 }
+		it { should be_owned_by 'root' }
+		it { should be_grouped_into 'root' }
+		its(:content) { should match /^log\_facility\=daemon$/}
+		its(:content) { should match /^pid\_file\=\/var\/run\/nrpe\/nrpe.pid$/}
+		its(:content) { should match /^server\_address\=(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d)$/ }
+		its(:content) { should match /^server\_port\=5666$/ }
+		its(:content) { should match /^nrpe\_user\=nrpe$/ }
+		its(:content) { should match /^nrpe\_group\=nrpe$/ }
+		its(:content) { should match /^allowed\_hosts\=(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d)\,(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d).(\d\d\d|\d\d|\d)$/ }
+		its(:content) { should match /^dont\_blame\_nrpe\=\d$/ }
+		its(:content) { should match /^debug\=\d$/ }
+		its(:content) { should match /^command\_timeout\=\d\d$/ }
+		its(:content) { should match /^connection\_timeout\=\d\d\d$/ }
+		its(:content) { should match /^include\_dir\=\/etc\/nrpe.d\/$/ }
+	end
